0c1c110a76f04df4e6503e0768463e012e6213d2
saltecrez <3lisa.londero@gmail.com>
2020-01-27 14:27:18 +0100

    based on class inheritance

--- share.py
+++ share.py
@@ -8,12 +8,10 @@ import yfinance as yf
 from datetime import date
 from datetime import timedelta
 
-class Share:
-    def __init__(self, label, buy_price, buy_date, wealth):
+class HistoricalData(object):
+    def __init__(self, label, buy_date):
         self.label = label
-        self.buy_price = buy_price
         self.buy_date = buy_date
-        self.wealth = wealth
         self.tomorrow = date.today() + timedelta(days=1)
 
     def get_historical_data(self):
@@ -21,5 +19,20 @@ class Share:
         hd = locator.history(start=self.buy_date,end=self.tomorrow)
         return hd
 
-if __name__ == "__main__":
-    Share("AFX.DE","93.123","2019-10-23","5005.8").get_historical_data()
+class TestInheritance(HistoricalData):
+    def __init__(self, label, buy_date, buy_price):
+        self.buy_price = buy_price
+        HistoricalData.__init__(self, label, buy_date)
+        self.d = HistoricalData.get_historical_data(self)
+
+    def get_close_price(self):
+        close = self.d['Close']
+        close_price = [close[i] for i in range(len(close))]
+        return close_price
+
+    def get_dates(self):
+        dates = [i for i in self.d.index]
+        return dates
+
+x = TestInheritance("AFX.DE","2019-10-23","93.123")
+print(x.get_close_price())
